12948e7de5234b7a4108156d96c6da85df34d8c4
CIVIBL <158622916+CIVIBL@users.noreply.github.com>
2025-09-13 14:31:30 -0400

    Configure Vite to proxy PHP requests

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/content-api.php': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
