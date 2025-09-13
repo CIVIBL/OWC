e4192fad0fd91b48bf90d2cb1dd204d41b45c760
CIVIBL <158622916+CIVIBL@users.noreply.github.com>
2025-09-13 14:33:14 -0400

    Fix vite.config.ts syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/admin': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
