a10ea4a535231874b2d0ce5142ae04aad24cbfcf
CIVIBL <158622916+CIVIBL@users.noreply.github.com>
2025-09-13 14:32:20 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
       '/admin': {
         target: 'http://localhost:8000',
         changeOrigin: true,
